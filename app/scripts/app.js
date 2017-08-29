d16bb00eebdbf30f120edc74df04efd680c97964
Quentin Curtet <quentincurtet@gmail.com>
2017-08-29 22:49:05 +0200

    [intern] : First Commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name srcAngularApp
+ * @description
+ * # srcAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('srcAngularApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name srcAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the srcAngularApp
+ */
+angular.module('srcAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
